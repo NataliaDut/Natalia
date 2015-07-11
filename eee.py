27fe39c4694fc23ed98b561f626251dca37db5f0
Natalia <dutkanatalia@wp.pl>
2015-07-11 16:04:50 +0200

    stworzono projekt

---
+++ eee.py
@@ -0,0 +1,6 @@
+def hi(name):
+	print('hi '+ name + "!")
+girls=['ala','ola','ela']
+for name in girls:
+	hi(name)
+	print('Next girl')
---
+++ liczba.py
@@ -0,0 +1,6 @@
+def porownaj (licz):
+	if (licz)>3:
+		print('it works!')
+	else:
+		print('fffff')
+porownaj(1)
